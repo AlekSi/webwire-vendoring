48d14f1eda716e1398dc317cef38f0fbb4cc0a62
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2018-03-29 10:12:54 +0300

    Update example for new webwire.

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
 	"github.com/qbeon/webwire-go"
 )
 
 func main() {
-	conn, _, _ := websocket.NewClient(nil, nil, nil, 0, 0)
-	_ = webwire.NewClientAgent(conn, "", nil)
+	_ = new(webwire.Client)
 }
